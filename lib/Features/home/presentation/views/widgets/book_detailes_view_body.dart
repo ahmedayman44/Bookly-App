db7cb2d73aba15b1e64912df83b7a5c6a207a696
ahmedayman44 <ahmedayman203011@gmail.com>
2024-07-11 01:44:25 +0300

    add custom book item to book detailes view

--- lib/Features/home/presentation/views/widgets/book_detailes_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_detailes_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:booklly_app/Features/home/presentation/views/widgets/custom_app_bar_book_detailes.dart';
+import 'package:booklly_app/Features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailesViewBody extends StatelessWidget {
@@ -6,14 +7,20 @@ class BookDetailesViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30),
+    // create a relative width to used in any device to save the volume of picture
+    var width = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
-          CustomAppBarBookDetailes(),
+          const CustomAppBarBookDetailes(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+            child: const CustomBookImage(),
+          ),
+          
         ],
       ),
     );
   }
 }
-
--- lib/Features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/Features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,4 +1,4 @@
-import 'package:booklly_app/Features/home/presentation/views/widgets/featured_list_view_item.dart';
+import 'package:booklly_app/Features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
@@ -14,7 +14,7 @@ class FeaturedBooksListView extends StatelessWidget {
           itemBuilder: (context, index) {
             return const Padding(
               padding: EdgeInsets.symmetric(horizontal: 8.0),
-              child: FeaturedListViewItem(),
+              child: CustomBookImage(),
             );
           }),
     );
