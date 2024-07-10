878559b6fa5568d8de75213d1158fbf8f5969c17
ahmedayman44 <ahmedayman203011@gmail.com>
2024-07-10 12:58:21 +0300

    Best Seller Item Part 2

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -5,6 +5,7 @@ import 'package:booklly_app/core/utils/style.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -17,7 +18,7 @@ class HomeViewBody extends StatelessWidget {
         CustomAppBar(),
         FeaturedBooksListView(),
         Padding(
-          padding: EdgeInsets.only(left: 24.0, top: 30, bottom: 15),
+          padding: EdgeInsets.only(left: 24.0, top: 30, bottom: 20),
           child: Text(
             'Best Seller',
             style: Styles.textStyle18,
@@ -34,28 +35,44 @@ class BestSellerListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 120,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.1 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(7),
-                image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(
-                    AssetsData.testImage,
+    return Padding(
+      padding: const EdgeInsets.only(left: 24.0),
+      child: SizedBox(
+        height: 120,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.1 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(7),
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      AssetsData.testImage,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          const Column(
-            children: [],
-          )
-        ],
+            const SizedBox(
+              width: 30,
+            ),
+            Column(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: const Text(
+                    'Harry Potter and the Globet of Fire',
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    style: Styles.textStyle20,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
