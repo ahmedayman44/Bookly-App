0631d25db8e446bddbdff5612a588a24ddfca611
ahmedayman44 <ahmedayman203011@gmail.com>
2024-07-13 14:36:36 +0300

    create custom search text field

--- lib/Features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/Features/home/presentation/views/widgets/custom_app_bar.dart
@@ -1,6 +1,8 @@
+import 'package:booklly_app/core/utils/app_router.dart';
 import 'package:booklly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -18,10 +20,12 @@ class CustomAppBar extends StatelessWidget {
           ),
           const Spacer(),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              GoRouter.of(context).push(AppRouter.kSearchView);
+            },
             icon: const Icon(
               FontAwesomeIcons.magnifyingGlass,
-              size: 21,
+              size: 18,
             ),
           )
           // Image.asset(
--- lib/Features/home/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/home/search/presentation/views/widgets/search_view_body.dart
@@ -1,10 +1,45 @@
+import 'package:booklly_app/Features/home/presentation/views/widgets/best_seller_list_view_item.dart';
+import 'package:booklly_app/Features/home/search/presentation/views/widgets/custom_search_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 14),
+      child: Column(
+        children: [
+          CustomSearchTextField(),
+          //SearchBestSellerListView(),
+        ],
+      ),
+    );
+  }
+}
+
+
+class SearchBestSellerListView extends StatelessWidget {
+  const SearchBestSellerListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        // shrinkWrap: true is forced to build all widget to make the list take the hight of these childern
+        //shrinkWrap: true,
+        // physics:  NeverScrollableScrollPhysics() to make the list not scrollable because it's not needed customScrollView scrollable
+        physics: const NeverScrollableScrollPhysics(),
+        padding: EdgeInsets.zero,
+        itemCount: 10,
+        scrollDirection: Axis.vertical,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: BestSellerListViewItem(),
+          );
+        });
   }
 }
