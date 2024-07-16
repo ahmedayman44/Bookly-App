e9e3289efb5e98d9ff002d76ec09877374df5f42
ahmedayman44 <ahmedayman203011@gmail.com>
2024-07-16 15:08:48 +0300

    craete featured books method

--- lib/Features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
+++ lib/Features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
@@ -11,6 +11,7 @@ final class FeaturedBooksInitial extends FeaturedBooksState {}
 
 final class FeaturedBooksLoading extends FeaturedBooksState {}
 
+//ال sucess قدامي اختيارين اخلي الداتا الي جتلي بالفعل عايشة في الcubit ذات نفسة او تجيلي من خلال الstate بتاعت الsucess انا بختار علي اساس هعرض البيانات فينانا لو هعرض البيانات داخل list يعني يدوب هديها للlist علطول فخلاص هستقبلها في الsucessفانا هنا بعتمد علي نقطة انا هعرض البيانات فين وعلي اساسها بحدد هل انا هستقبل البيانات داخل الsucess state  ولا هخليها جوة الcubit
 final class FeaturedBooksSuccess extends FeaturedBooksState {
   final List<BookModel> books;
   const FeaturedBooksSuccess(this.books);
